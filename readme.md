c7ea6617dbf045125b3105e5c90454301175c101
Thijs Vermeir <thijsvermeir@gmail.com>
2018-09-02 20:44:36 +0200

    expect string slice as filename and cleanup API

--- readme.md
+++ readme.md
@@ -1,5 +1,6 @@
 # Gradle-Sync
 [![Build Status](https://travis-ci.org/lovebug356/gradle-sync.svg?branch=master)](https://travis-ci.org/lovebug356/gradle-sync)
+[![Latest version](https://img.shields.io/crates/v/gradle-sync.svg)](https://crates.io/crates/gradle-sync)
 
 **Small utility to synchronize the gradle version with the cargo version.**
 
@@ -9,12 +10,20 @@ Add build dependency in ```Cargo.toml```:
 
 ```toml
 [build-dependencies]
-gradle-sync = 0.1.0
+gradle-sync = 0.1.4
+
 ```
 
 Add the following code snipper to ```build.rs```:
 
 ```[rust]
+extern crate gradle_sync;
+use gradle_sync::GradleFile;
+
+fn main() {
+    GradleFile::new_and_sync_with_cargo("./app/build.gradle").unwrap();
+}
+
 ```
 
 ## License
--- src/file.rs
+++ src/file.rs
@@ -2,6 +2,7 @@ use error::GradleResult;
 use error::Error;
 use buffer::GradleBuffer;
 use std::fs::File;
+use std::env;
 use std::fs::OpenOptions;
 use semver::Version;
 
@@ -11,16 +12,25 @@ pub struct GradleFile {
 }
 
 impl GradleFile {
-    pub fn new(filename: String) -> GradleResult<Self> {
-        let fd = File::open(&filename)
+    pub fn new(filename: &str) -> GradleResult<Self> {
+        let fd = File::open(filename)
             .map_err(|_err| {
                 let reason = format!("failed to read file: {}", filename);
                 Error::IoError(reason)
             })?;
         let buffer = GradleBuffer::from(fd)?;
         Ok(Self {
-            filename, buffer
+            filename: filename.to_string(),
+            buffer
         })
+    }
+
+    pub fn new_and_sync_with_cargo(filename: &str) -> GradleResult<()> {
+        let pkg_version = env::var("CARGO_PKG_VERSION").unwrap();
+        let mut gradle_file = GradleFile::new(filename)?;
+        gradle_file.synchronize_version(&pkg_version)?;
+        gradle_file.write()?;
+        Ok(())
     }
 
     pub fn synchronize_version(&mut self, version_name: &str) -> GradleResult<()> {
